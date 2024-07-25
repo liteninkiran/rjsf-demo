766cb5d03bc28b92fd322750b35469b363eb8150
Kiran Anand <liteninkiran@hotmail.com>
2024-07-25 15:11:40 +0100

    Custom error messages

--- src/components/Demo.tsx
+++ src/components/Demo.tsx
@@ -3,25 +3,27 @@ import { RJSFSchema } from '@rjsf/utils';
 import validator from '@rjsf/validator-ajv8';
 
 const Demo = () => {
+  function transformErrors(errors: any, uiSchema: any) {
+    return errors.map((error: any) => {
+      if (error.name === 'pattern') {
+        error.message = 'Only digits are allowed';
+      }
+      return error;
+    });
+  }
+
   const schema: RJSFSchema = {
-    title: 'Todo',
     type: 'object',
-    required: ['title'],
     properties: {
-      title: { type: 'string', title: 'Title', default: 'A new task' },
-      done: { type: 'boolean', title: 'Done?', default: false },
+      onlyNumbersString: { type: 'string', pattern: '^\\d*$' },
     },
   };
-
-  const log = (type: any) => console.log.bind(console, type);
 
   return (
     <Form
       schema={schema}
       validator={validator}
-      onChange={log('changed')}
-      onSubmit={log('submitted')}
-      onError={log('errors')}
+      transformErrors={transformErrors}
     />
   );
 };
